cb334620a44c172bcf4e7c509bde8a2bd02845a4
Muqingluan <hhjver@gmail.com>
2018-11-23 11:37:13 +0800

    add status check module

---
+++ statucheck.py
@@ -0,0 +1,10 @@
+import os
+import time
+
+if __name__ == "__main__":
+
+    while True:
+        if os.system('ps -ef | grep main.py') == null:
+            os.system("python main.py")
+
+        time.sleep(2)
