d413bf3a4a5bb1cfb63083f3e9cc02ec3825da6b
Muqingluan <hhjver@gmail.com>
2018-11-23 11:31:05 +0800

    add checkjiagu case

--- yingyongbao.py
+++ yingyongbao.py
@@ -16,6 +16,8 @@ def load(path):
 
 
 def yingyongbaodownload(path):
+
+    print "save path is " + path
     apkfile_store = []
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'
