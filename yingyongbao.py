6890488e01b881c1ea86247db26c1cef3c4bc6b0
Muqingluan <hhjver@gmail.com>
2018-11-23 11:22:03 +0800

    add checkjiagu case

--- yingyongbao.py
+++ yingyongbao.py
@@ -44,7 +44,7 @@ def yingyongbaodownload(path):
         parserjson = json.loads(response.read())
         if parserjson is None:
             continue
-        print parserjson
+        #print parserjson
 
         time.sleep(1)
 
