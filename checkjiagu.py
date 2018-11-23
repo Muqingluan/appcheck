4c6d73f9d4901bb847f4313084facdd0c0761870
Muqingluan <hhjver@gmail.com>
2018-11-23 11:07:17 +0800

    add checkjiagu case

--- checkjiagu.py
+++ checkjiagu.py
@@ -3,7 +3,33 @@ import zipfile
 import sys,os
 import json
 
+
 jiagu_magic = [
+'.appkey',
+'libjiagu.so',
+'libjiagu-x86.so',
+'SecShell0.jar',
+'libSecShell.so',
+'classes0.jar',
+'libDexHelper.so',
+'libdexjni.so',
+'dx-enc.data',
+'dx-resp.data',
+'libdxn.so',
+'libjdi.so',
+'libx3g-ind.so',
+'libx3g-ld.so',
+'libstee.so',
+'libx3g.so',
+'af.bin',
+'ijiami.ajm',
+'libexec.so',
+'af.bin',
+'libexec.so',
+'libexecmain.so',
+'classes.dex',
+'libhdog.so',
+'libvdog.so',
 'libexecdll.so',
 'libexecdllgame.so',
 'ijm_lib',
@@ -28,7 +54,8 @@ jiagu_magic = [
 'libkdp.so',
 'libkdp.so',
 'libCmbShield.so',
-'libCmbShield.x86.so']
+'libCmbShield.x86.so'
+]
 
 
 if __name__ == "__main__":
